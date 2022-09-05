c960004c38de215cd9617a00a530dd65b42042ce
Rex61 <kotrex96@gmail.com>
2022-09-05 21:04:08 +0300

    init commit

---
+++ src/main/java/ru/agasbek/learnapp/LearnAppApplication.java
@@ -0,0 +1,13 @@
+package ru.agasbek.learnapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/agasbek/learnapp/LearnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.agasbek.learnapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
