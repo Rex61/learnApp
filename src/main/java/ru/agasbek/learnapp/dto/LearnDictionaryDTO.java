038c0999595ecdb7af4da4e0d7bd7d739e49aef5
Rex61 <kotrex96@gmail.com>
2022-10-03 21:36:01 +0300

    mySql schema config

--- src/main/java/ru/agasbek/learnapp/dto/LearnDictionaryDTO.java
+++ src/main/java/ru/agasbek/learnapp/dto/LearnDictionaryDTO.java
@@ -2,12 +2,9 @@ package ru.agasbek.learnapp.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import ru.agasbek.learnapp.model.LearnCard;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
 
 /**
  * A DTO for the {@link ru.agasbek.learnapp.model.LearnDictionary} entity
@@ -16,6 +13,5 @@ import java.util.Set;
 @Builder
 public class LearnDictionaryDTO implements Serializable {
     private long id;
-    private Set<LearnCard> cards;
     private LocalDateTime lastSeen;
 }
